e47cd2c3b4f83ff7bacc2e198c28c537094f037a
Chu-rill <churchillrimamskep@gmail.com>
2024-09-13 07:39:21 +0100

    removed some code

--- src/validation/leaderboard.js
+++
@@ -1,8 +0,0 @@
-const joi = require("joi");
-
-exports.submit_score_validator = joi.object({
-  userId: joi.object().required(),
-  category: joi.string().required(),
-  score: joi.number().required(),
-  quizId: joi.string().required(),
-});
