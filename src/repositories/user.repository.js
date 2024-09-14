d3b18fb82cdf014bab82221fb7258c80e0b4077b
Chu-rill <churchillrimamskep@gmail.com>
2024-09-14 02:55:35 +0100

    added a retunr in the user repository file

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,8 +1,24 @@
 const User = require("../models/User");
 
-const getUserByUsername = async (username) => User.findOne({ username });
-const createUser = async (userData) => User.create(userData);
-const deleteUserById = async (userId) => User.findByIdAndDelete(userId);
-const getAllUsers = async () => User.find();
+const getUserByUsername = async (username) => {
+  const user = await User.findOne({ username });
+  return user;
+};
+const createUser = async (username, password, email) => {
+  const user = await User.create({
+    username,
+    password,
+    email,
+  });
+  return user;
+};
+const deleteUserById = async (userId) => {
+  const user = await User.findByIdAndDelete(userId);
+  return user;
+};
+const getAllUsers = async () => {
+  const user = await User.find();
+  return user;
+};
 
 module.exports = { getUserByUsername, createUser, deleteUserById, getAllUsers };
